fe5f6f8a5a1c85d4180df83b558ccf3a62c902f2
Gisele Aisaeva <gisel.aisaeva@gmail.com>
2023-11-28 15:41:27 +0000

    Created a function called getCountryByName

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const getCountryByName = async (countryName) => {
+    const response = await fetch("https://restcountries.com/v3.1/name/" +countryName);
+    const data = await response.json();
+    console.log(data);
+}
+
+getCountryByName("Brazil");
